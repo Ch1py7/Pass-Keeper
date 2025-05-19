3c6fa475cdaf35da350a13f6a3922971c2f66a7f
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-05-18 20:58:25 -0600

    refactor: move search input to a separate div for better structure

--- src/components/UnlockedFile.tsx
+++ src/components/UnlockedFile.tsx
@@ -14,23 +14,23 @@ export const UnlockedFile = () => {
 	return (
 		<div className='max-w-7xl mx-auto flex flex-col items-center'>
 			<Header isCompact={isCompact} setIsCompact={setIsCompact} />
+			<div className='relative w-full max-w-lg flex items-center'>
+				<Icon
+					icon='radix-icons:magnifying-glass'
+					className='absolute left-3 h-5 w-5 text-slate-400 mt-1'
+				/>
+				<input
+					type='search'
+					placeholder='Search passwords...'
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+					className='pl-10 h-12 rounded-full w-full border-1 border-solid border-slate-300 shadow-md focus:ring-2 focus:ring-purple-500 focus:border-transparent select-none'
+				/>
+			</div>
 			<div
 				ref={canvasContainerRef}
 				className='flex flex-col md:flex-row items-center md:items-start h-full w-full pt-6 pb-8 px-10 gap-6'
 			>
-				<div className='relative w-full max-w-lg flex items-center'>
-					<Icon
-						icon='radix-icons:magnifying-glass'
-						className='absolute left-3 h-5 w-5 text-slate-400 mt-1'
-					/>
-					<input
-						type='search'
-						placeholder='Search passwords...'
-						value={searchQuery}
-						onChange={(e) => setSearchQuery(e.target.value)}
-						className='pl-10 h-12 rounded-full w-full border-1 border-solid border-slate-300 shadow-md focus:ring-2 focus:ring-purple-500 focus:border-transparent select-none'
-					/>
-				</div>
 				<SelectionBox />
 				<Categories />
 				<EntryList searchQuery={searchQuery} isCompact={isCompact} />
