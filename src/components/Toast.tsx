dc42275edebb8cdb09f5c083676441e797aec9a1
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-05-26 10:17:11 -0600

    feat: update Toast component by removing icon functionality and enhancing CSS variables for better theming

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,4 +1,3 @@
-import { Icon } from '@iconify/react'
 import { Slide, ToastContainer } from 'react-toastify'
 
 export const Toast = () => {
@@ -8,20 +7,7 @@ export const Toast = () => {
 			autoClose={5000}
 			closeOnClick
 			transition={Slide}
-			icon={({ type }) => {
-				switch (type) {
-					case 'info':
-						return <Icon icon='lucide:info' className='text-indigo-500 h-5 w-5' />
-					case 'error':
-						return <Icon icon='lucide:circle-alert' className='text-red-500 h-5 w-5' />
-					case 'success':
-						return <Icon icon='lucide:circle-check' className='text-green-500 h-5 w-5' />
-					case 'warning':
-						return <Icon icon='lucide:triangle-alert' className='text-yellow-500 h-5 w-5' />
-					default:
-						return null
-				}
-			}}
+			theme='colored'
 			className='p-4 gap-2 toast:p-0 toast:gap-0'
 			limit={3}
 		/>
