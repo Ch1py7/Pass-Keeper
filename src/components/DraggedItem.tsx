272fd99da0102897637aaf1bbc565529ef7a775a
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-05-26 10:22:39 -0600

    feat: update DraggedItem component to use CSS variables for theming; remove unused custom variant in CSS

--- src/components/DraggedItem.tsx
+++ src/components/DraggedItem.tsx
@@ -5,7 +5,7 @@ export const DraggedItem = forwardRef<HTMLDivElement>((_, ref) => {
 	const { selectedItems } = useFileStore()
 	return (
 		<div
-			className='-translate-x-[2000px] -translate-y-[2000px] -z-50 absolute w-12 h-12 rounded-full flex items-center justify-center bg-gradient-to-r from-purple-600 to-pink-600 font-bold'
+			className='-translate-x-[2000px] -translate-y-[2000px] -z-50 absolute w-12 h-12 rounded-full flex items-center justify-center bg-gradient-to-r from-[var(--theme-bg-secondary)] to-[var(--theme-bg-primary)] font-bold'
 			ref={ref}
 		>
 			{selectedItems.length}
