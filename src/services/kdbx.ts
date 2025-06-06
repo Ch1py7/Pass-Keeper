67127dc2951d66cdaa8a25461a164f2c7312f2bc
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-06-06 17:48:35 -0600

    feat: remove debug log from deleteEntry method in Kdbx class

--- src/services/kdbx.ts
+++ src/services/kdbx.ts
@@ -95,7 +95,6 @@ export class Kdbx {
 
 	public async deleteEntry(data: ManageEntry, save = true) {
 		const entry = this._getEntryById(data)
-		console.log(entry)
 		this._requireDb().remove(entry)
 		save && this.persist()
 	}
